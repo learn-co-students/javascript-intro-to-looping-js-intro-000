4b285122c3afb6a9d5aa846c1c94c51a23b1f725
Charles Pletcher <cpletcher2@gmail.com>
2016-05-11 15:49:04 -0400

    Update solution

--- loops.js
+++ loops.js
@@ -1,26 +1,35 @@
-function loopExcitement(){
-  var sentences = []
-  for (var i = 50; i < 75; i++){
-    sentences.push("i am looping");
-  }
-  return sentences;
+function forLoop(array) {
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
 }
 
-function loopingFun(){
-  var i = 10;
-  var num;
-  while(i < 17){
-    num = i;
-    i++;
-  }
-  return num;
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
+
+	return 'done';
 }
 
-function alwaysLooping(num){
-  var love = []
-  do {
-    love.push("JavaScript")
-    num ++;
-  } while (num < 10);
-  return love;
+function doWhileLoop(array) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+	}
+
+	do {
+		console.log('array.length = ' + array.length + ' and i = ' + i);
+		array = array.slice(1);
+		incrementVariable();
+	} while (array.length > 0 && i < 5);
+
+	return array;
 }
