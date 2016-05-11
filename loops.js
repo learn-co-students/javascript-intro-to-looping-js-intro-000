19b30df97273ed65a18d3f5b02b1f8cffb61d708
Charles Pletcher <cpletcher2@gmail.com>
2016-05-11 15:49:04 -0400

    Update solution

--- loops.js
+++ loops.js
@@ -1,35 +1,32 @@
 function forLoop(array) {
-	for (let i = 0; i < 25; i++) {
-		if (i === 1) {
-			array.push('I am 1 strange loop.');
-		} else {
-			array.push(`I am ${i} strange loops.`);
-		}
-	}
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
 
-	return array;
+  return array
 }
 
-function whileLoop(n) {
-	while (n > 0) {
-		console.log(--n);
-	}
 
-	return 'done';
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+
+  return 'done'
 }
 
 function doWhileLoop(array) {
-	var i = 0;
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
 
-	function incrementVariable() {
-		i = i + 1;
-	}
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
 
-	do {
-		console.log('array.length = ' + array.length + ' and i = ' + i);
-		array = array.slice(1);
-		incrementVariable();
-	} while (array.length > 0 && i < 5);
-
-	return array;
+  return array
 }
