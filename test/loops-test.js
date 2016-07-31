fbe8592f0edc9d7d91c56791c23c9f63c9c28cfd
Braden Jonsson <braden337@gmail.com>
2016-07-31 16:25:23 -0500

    fix wrong function name in test description

--- test/loops-test.js
+++ test/loops-test.js
@@ -54,7 +54,7 @@ describe('loops', () => {
     })
   })
 
-  describe('doWhile(array)', () => {
+  describe('doWhileLoop(array)', () => {
     it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
       const [array, t] = makeArray()
       const l = array.length
