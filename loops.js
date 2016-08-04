d98cbf9416b0162f482d673faae8a73fa7fdf50d
Jessica Fischbach <jfisch9@gmail.com>
2016-08-04 20:53:40 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push("I am " + i + " strange loops.");
+    }
+  }
+  return array;
+}
+
+
+function whileLoop(num) {
+  while(num > 0) {
+    console.log(--num);
+  }
+
+  return "done";
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  }while(maybeTrue());
+  return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
