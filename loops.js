33043361daed4565773ac1c47f20df07b028df75
Niky Morgan <niky.morgan@gmail.com>
2016-08-07 03:45:43 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+    return array;
+  } while (array.length > 0 && maybeTrue());
+}
+
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+      array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
+  }
+  return array;
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return 'done';
+}
