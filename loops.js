bcabc8a696bfbfc672070d4c76dab63ba5304477
Steven Covington <dlinetackle@yahoo.com>
2016-08-08 23:27:09 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,9 +3,25 @@ function forLoop(array) {
     if (i === 1) {
     array.push ("I am 1 strange loop.")
   }
-    else if  (i >== 2 || i === 0) {
+    else {
     array.push ("I am ${i} strange loops.")
   }
 }
 return array
+}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log (--n)
+  }
+  return 'done'
+}
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.shift()
+  } while (array.length > 0 && maybeTrue());
+  return array;
 }
