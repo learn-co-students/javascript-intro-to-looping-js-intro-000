806644efece9c73116c61a07c38abec954e365e8
Steven Covington <dlinetackle@yahoo.com>
2016-08-08 23:14:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+    array.push ("I am 1 strange loop.")
+  }
+    else if  (i >== 2 || i === 0) {
+    array.push ("I am ${i} strange loops.")
+  }
+}
+return array
+}
