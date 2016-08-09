a2a01030130f5b20caa5c6f8cf26aa750223bc8a
Mitch Masterson <mitchell.masterson@gmail.com>
2016-08-09 11:42:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,31 @@
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
 function forLoop(array) {
   for (i = 1; i <= 25; i++) {
-    if (i == 1) {
+    if (i === 1) {
       array.push(`I am ${i} strange loop.`)
     } else {
       array.push(`I am ${i} stange loops.`)
     }
   }
+
   return array
 }
 
 function whileLoop(n) {
-  for (i = n; i > 0; i--) {
-    console.log(i)
+  while (n > 0) {
+    console.log(n--)
   }
+
   return 'done'
 }
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
   do  {
-    array.pop()
-  } while (maybeTrue() || array.length > 0)
-  return array
+    newArray = array.slice(1)
+  } while (maybeTrue() && array.length > 0)
+
+  return newArray
 }
