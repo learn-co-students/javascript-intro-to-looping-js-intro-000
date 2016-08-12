ace7f8aea7eb7cb69b0f3ea2f60e93a2065920ee
Karina Bartz <ksbartz@gmail.com>
2016-08-12 16:26:03 +0000

    Done.

--- loops.js
+++ loops.js
@@ -12,12 +12,5 @@ function whileLoop(n){
 }
 
 function doWhile(array){
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
 
-  do {
-    console.log('doo-bee-doo-bee-doo')
-  } while (maybeTrue());
-array.length > 0 && maybeTrue()
 }
