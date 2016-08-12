37fcdb434648b3cee416be9a95777dffb2bc807e
Karina Bartz <ksbartz@gmail.com>
2016-08-12 17:08:33 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,14 +1,17 @@
-var array = [];
-
 function forLoop(array) {
   for (let i = 0; i < 25 ; i++){
-    return i === 0 ? "I am 1 strange loop." : `I am + ${i} + strange loops.`
+    i === 0 ? array.push(["I am 1 strange loop."]) : array.push([`I am + ${i} + strange loops.`]);
   }
+  return (array);
 };
 
-
+/*
 function whileLoop(n){
+  let countdown = (n-1);
 
+  while (countdown > 0) {
+    console.log('done')
+  }
 }
 
 function doWhile(array){
