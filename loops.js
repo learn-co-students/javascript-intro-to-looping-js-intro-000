70e846e042a9f9f5c801468d81137626e04848f4
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 20:23:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -18,3 +18,14 @@ function whileLoop(number){
   return "done";
 
 };
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length > 0 && maybeTrue());
+  return array;
+};
