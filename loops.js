ec51444ba15d13b9ca7dde86bc826e8c35eb8426
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 19:50:34 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(arr){
-  for (let i=0; i<25; i++){
-    if (i=1){
-      arr.push("I am 1 strange loop.");
+function forLoop(array){
+  for (var i=0; i<25; i++){
+    if (i===1){
+      array.push("I am 1 strange loop.");
     }
     else{
-      arr.push("I am ${i} strange loops.");
+      array.push(`I am ${i} strange loops.`);
     }
   }
 };
