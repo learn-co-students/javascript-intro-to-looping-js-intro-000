488cc8dcd8dd694f519ed2da5276ba46cf4fab5e
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-14 21:07:22 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,12 +1,10 @@
 
 function forLoop(array){
-
   for (var i=0;i<25;i++){
-      if i===1 {array.push ("I am " + ${i}+ " strange loop")}
+      if (i===1 ){array.push ("I am " + ${i} + " strange loop")}
       else {
         array.push("I am " + ${i}+ "strange loop${i=== 0? '': 's'}.")
       }
   }
-  console.log(array)
   return array
 }
