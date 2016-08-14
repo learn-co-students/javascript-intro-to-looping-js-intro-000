a8c3f362c38c58b070b2d4929654579742a5fae3
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-14 20:54:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+
+function forLoop(array){
+
+  for (var i=0;i<25;i++){
+      if i===1 {array.push ("I am " + ${i}+ " strange loop")}
+      else {
+        array.push("I am " + ${i}+ "strange loop${i=== 0? '': 's'}.")
+      }
+  }
+  console.log(array)
+  return array
+}
