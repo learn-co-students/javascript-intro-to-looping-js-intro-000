dbf41f8933619d5a677f4cb9710a6da01024a18b
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-15 04:07:42 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,10 +2,35 @@
 function forLoop(array){
   for (var i=0;i<25;i++){
       if (i===1 ){
-        array.push("I am " + ${i} + " strange loop")
+        array.push("I am " + i + " strange loop")
       } else {
-        array.push("I am " + ${i}+ "strange loop${i=== 0? '': 's'}.")
+        array.push("I am " + i+ "strange loop${i=== 0? '': 's'}.")
       }
   }
   return array
+}
+
+function whileLoop(n){
+  while(n>0)
+  {
+    n=n-1
+  }
+return('done')
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+function doWhileLoop(array)
+{
+
+  {
+    array.pop()
+  }
+  while
+
+      (array.length>0 && maybeTrue())
+
+  return array
+  console.log(array.length)
 }
