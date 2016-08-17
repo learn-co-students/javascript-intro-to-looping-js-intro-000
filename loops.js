f450e0fefaa0ec900305593e1eb98f8fdf7a8929
Elsa Brown <elsambrown@gmail.com>
2016-08-17 16:21:55 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  for (var i = 0; i <= 25; i++) {
+    if (i === 1) {
+      return "I am 1 strange loop.";
+    }
+    else {
+      return "I am ${i} strange loops.";
+    }
+  }
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number);
+  return "done";
+  }
+}
