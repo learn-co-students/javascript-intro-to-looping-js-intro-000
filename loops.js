8e4e14fbf94f4eb18ac22ec55f9813fdea6c4bf7
Cindy Gao <cindygao@hotmail.com>
2016-08-26 22:24:18 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array){
+  for (let i = 0; i<25; i++){
+    if (i = 1){
+      array.push(`I am ${i} strange loop.`)
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+}
+
+function whileLoop(n){
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  while(array.length > 0 && maybeTrue()){
+    array.pop()
+  }
+}
