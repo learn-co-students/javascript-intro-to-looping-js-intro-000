4d1393580604b4e7fad87ccddf206e9c4ce60849
imani fecu <imani.fecu72@ymail.com>
2016-08-29 15:45:54 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,12 +1,19 @@
-var array = []
 function forLoop(array){
-  for (var i=0; i<25, i++){
-    if (i>1){
-      array.push(`I am ${i} strange loops.`)
-  }
-    else{
-      array.push(`I am ${i} strange loop.`)
+  for (var i=0; i<25, i++;){
+    if (i===1)
+    {
+      array.push(`I am ${i} strange loop.`);
+    }
+    else
+    {
+      array.push(`I am ${i} strange loops.`);
     }
   }
-  return array
+  return array;
+}
+function whileLoop(n){
+  while (n!==0){
+  console.log(--n);
+  }
+  return "done";
 }
