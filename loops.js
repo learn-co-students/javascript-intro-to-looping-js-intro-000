5f998592f6dca6b223433686fc7e50331c04c5d4
Fernando Gomez <fhergomez37@gmail.com>
2016-09-13 17:03:23 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,32 @@
 function forLoop(array){
   for(var i=0;i<25;i++){
     if(i===1){
-      array[`"I am ${i} strange loop"`];
+      array.push(`"I am ${i} strange loop"`);
     } else {
-      array[`"I am ${i} strange loops"`];
+      array.push(`"I am ${i} strange loops"`);
     }
   }
+  return array
 }
+
+function whileLoop (number){
+  var counter = number;
+  while (counter > 0){
+    counter--;
+    console.log(counter);
+  }
+  return "done";
+}
+
+
+
+// function doWhile(array){
+//   function maybeTrue() {
+//     return Math.random() >= 0.5
+//   }
+//   do {
+//     array.length - 1;
+//   } while {
+//     array.length > 0 && maybeTrue();
+//   }
+// }
