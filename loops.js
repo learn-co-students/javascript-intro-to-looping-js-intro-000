b8f29d8baf4b86a7a9187639f63b4590620c0239
CHRISTOPHER STAIR <ccstair@gmail.com>
2016-09-19 20:24:17 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+  var array = [];
+
+function forLoop(array) {
+  for (i=0;i<25;i++) {
+      array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while(n>0) {
+    console.log(n)
+    n--
+  }
+  return 'done';
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+return array;
+}
