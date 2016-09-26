89a41d33bd250bee671cc4174308693433384de1
Tayari Appiah <cjhhh.ta@gmail.com>
2016-09-26 20:11:55 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,7 +2,6 @@ function forLoop(array){
   for(let i = 0; i < 25; i++){
     if(i = 1){
      array.push("I am 1 strange loop.")
-      return array
   }
     else{
       array.push(`I am ${i} strange loops.`)
