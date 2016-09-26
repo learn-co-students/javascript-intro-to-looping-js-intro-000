ec902a8165f353b7ce0ebc6c524ed11a2524a49b
Tayari Appiah <cjhhh.ta@gmail.com>
2016-09-26 18:00:47 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if(i = 1){
+     array.push("I am 1 strange loop.")
+      return array
+  }
+    else{
+      array.push(`I am ${i} strange loops.`)
+        return array
+    }
+  }
+}
