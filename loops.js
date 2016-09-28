50185972a1e9ac76dc55ceabd3e52243373a2cf5
Sarah Bartley <sbartley1080@gmail.com>
2016-09-28 01:32:29 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am " + i + " strange loop.")
+    } else {
+      array.push("I am " + i +" strange loops${i===0? '': 's'}.")
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+  n=n-1
+  }
+
+  return('done')
+}
+
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+  console.log(array.length)
+}
