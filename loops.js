8ad033281ca7691e90b41b5c0ea40679a605e82c
p sc <patty.stacruz@gmail.com>
2016-09-30 17:00:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+    for (var i = 0; i < 25; ++i) {
+      if (i === 1) {
+        array.push("I am 1 strange loop.")
+      }
+      else{
+        array.push("I am ${i} strage loops.")
+      }
+    }
+    return array
+}
+
+function whileLoop(n) {
+  while(n>0){
+    console.log(n)
+    --n
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  function maybeTrue(){
+    return Math.random() >= 0.5;
+  }
+
+  do{
+    array = array.pop
+  } while (array.length>0 && maybeTrue());
+  return array
+}
