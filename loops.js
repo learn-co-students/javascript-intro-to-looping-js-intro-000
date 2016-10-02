52882aaabae06b328dc81bc35101fcfa3b30dde4
Nick James <nickjames640@gmail.com>
2016-10-02 18:40:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array) {
+  for (i = 0; i<25; i++) {
+    if (i === 1) { array.push(`I am a strange loop.`)}
+    else {array.push(`I am ${i} strange loops.`)}
+    }
+    return array
+}
+
+
+function whileLoop (n) {
+  while (n >0) {
+    console.log(n)
+    n-=1
+      }
+    return 'done'
+}
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+function doWhileLoop(array){
+do {array.shift()}
+
+  while(array.length > 0 && maybeTrue());
+return array}
