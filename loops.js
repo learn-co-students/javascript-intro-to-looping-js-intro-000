c6cd8fe4f927c499221297a20c44853d52404f30
Jane Claire Remick <jdotcdot@hotmail.com>
2016-10-09 22:07:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+
+
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while (n > 0){
+    console.log(n);
+    n = n-1;
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do {
+  array.pop();
+} while (array.length > 0 && maybeTrue());
+return array;
+}
