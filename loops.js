7a850025ce18a9b6216a258a6d8ce72ab2498811
nikymorg <nicole.morgan@gmail.com>
2016-11-03 19:10:35 -0400

    Redone

--- loops.js
+++ loops.js
@@ -1,25 +1,25 @@
-function maybeTrue() {
-  return Math.random() >= 0.5
+function forLoop(array) {
+  for (let i = 1; i <= 25; i++) {
+  array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n--
+  }
+  return "done"
 }
 
 function doWhileLoop(array) {
   do {
-    array.pop();
-    return array;
-  } while (array.length > 0 && maybeTrue());
+    array.pop()
+  } while (maybeTrue());
+  return array
 }
 
-function forLoop(array) {
-  for (i = 0; i < 25; i++) {
-      array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
-  }
-  return array;
-}
-
-
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(n--);
-  }
-  return 'done';
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
