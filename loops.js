e02c3bfbb0d6aa74ded3838a9f3bb84c7772c913
veechi tour <coreygames88@gmail.com>
2016-11-05 23:50:18 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+var m = [array].length
+for (var i = 0; i < 25; i++ ){
+if (i === 1 ) {
+  return "I am 1 strange loop."
+
+} else if (i < 25 ; i++) {  return `I am ${i} strange loops.`
+
+
+    }
+}
+}
