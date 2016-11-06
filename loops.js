ced070b1e37af251f255b81789892c7a79aa726e
veechi tour <coreygames88@gmail.com>
2016-11-06 00:27:57 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
 function forLoop(array){
 for (var i = 0; i < 25; i++ ){
 if (i === 1 ) {
-  return array.push("I am 1 strange loop.")
+  array.push("I am 1 strange loop.")
 
-} else {  return array.push(`I am ${i} strange loops.`)
+
+} else { array.push(`I am ${i} strange loops.`)
+
 
 
     }
