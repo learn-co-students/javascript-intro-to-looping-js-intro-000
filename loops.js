35ddb659f6fb8506d8d06bf2f06729a0e369959e
Amit Singh <amit_nyc@live.com>
2016-11-15 18:25:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,4 +1,15 @@
-
+function forLoop(array){
+for (let i = 0;  i <25; i++){
+if (i===1){
+  array[i]="I am 1 strange loop."
+}
+  else
+  {
+    array[i]="I am "+i+"strange loops."
+  }
+}
+  return array
+}
 function whileLoop(n){
   while(n>0){
     console.log(n)
