a8f81be686f87937c528abe0b05a1508dffb43d8
Ines Pucker <ines.pucker@gmx.de>
2016-11-18 17:35:41 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop (array) {
+for (var i=0; i < 25; i++)
+{
+if (i==1) {
+array.push ('I am 1 strange loop.');
+}
+else {
+array.push ("I am " + i + " strange loops.");
+}
+}
+return array
+}
+
+function whileLoop (number) {
+while (number > 0){
+console.log(number--)
+}
+return "done"
+}
+
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop (array) {
+do{
+array.pop ();
+}
+while (maybeTrue() && array.length > 0 );
+return array
+}
