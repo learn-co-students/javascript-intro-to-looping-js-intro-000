a771951c766f46d18d436a72dbe60f47d602956d
Siobhan Harley <siobhan.harley@gmail.com>
2016-12-23 00:02:31 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop (arr) {
+  for(i = 0; i < 25; i++) {
+      if(i === 1) {
+    arr.push("I am 1 strange loop.")
+    }
+    else {
+      arr.push("I am ${i} strange loops.")
+    }
+  }
+  return arr;
+}
+
+function whileLoop (n) {
+  while(n > 0) {
+    console.log(--n);
+  }
+  return "done";
+}
+
+
+function doWhileLoop (array) {
+  function maybeTrue() {
+      return Math.random() >= 0.5
+    }
+
+  do {
+       --array.length;
+  } while ( array.length > 0 && maybeTrue());
+return array;
+}
