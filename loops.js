213fdf40d186ba023bf9a9f5d8c10e050d6ccbf9
Walker Jorge Sousa de Souza <walker.souza@gmail.com>
2017-02-03 20:25:10 +0000

    Done.

--- loops.js
+++ loops.js
@@ -2,15 +2,12 @@ var array = [];
 function forLoop(array){
   for (let i = 0; i < 25; i++){
 
-    if (i == 1){
-      array[i] = "I am 1 strange loop.";
-
-    } else {
-      for (var j = 1, j<=25, j++) {
-        array[i] = `I am ${i} strange loops.`;
-      }      
-    }
+    if (i === 1){
+      array.push("I am 1 strange loop.")
+  } else {
+    array.push(`I am ${i} strange loops.`)
   }
+}
   return array
 }
 function whileLoop(number){
