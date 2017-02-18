cc77fbcf333b442f5607c646babef4a045c6d630
Jessica Blake <jcblake14@gmail.com>
2017-02-18 17:01:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 0) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (maybeTrue() === true);
+  return array;
+}
