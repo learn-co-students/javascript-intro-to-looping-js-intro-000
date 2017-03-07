ce5527d6ac59e14630f0cef77346c841abdd11dd
Michal Vacek <vahyno@gmail.com>
2017-03-07 19:58:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,29 @@
-function forLoop(array){
+function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  if (i=1) {return "I am 1 strange loop."}; 
-  else {return( "I am ${i} strange loops." )};
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
+  return array
+}
+
+function whileLoop(n) {
+ while (n > 0) {
+  console.log("${n}");
+  n = n-1};
+  return('done')
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
 }
--- test/loops-test.js
+++ test/loops-test.js
@@ -27,41 +27,41 @@ describe('loops', () => {
   })
 
   describe('forLoop(array)', () => {
-  //  it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-  //    const [array, t] = makeArray()
-  //    const strangeArray = forLoop(array)
+    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
+      const [array, t] = makeArray()
+      const strangeArray = forLoop(array)
 
-//      expect(strangeArray.length).to.equal(t + 25)
+      expect(strangeArray.length).to.equal(t + 25)
 
-//      const testArray = strangeArray.slice(array.length)
+      const testArray = strangeArray.slice(array.length)
 
-//      for (let i = 0, l = testArray.length; i < l; i++) {
-//        let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
-//        expect(testArray[i]).to.equal(s)
-//      }
-//    })
+      for (let i = 0, l = testArray.length; i < l; i++) {
+        let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+        expect(testArray[i]).to.equal(s)
+      }
+    })
   })
 
   describe('whileLoop(n)', () => {
-//    it('counts down from n to 0', () => {
-//      const spy = chai.spy.on(console, 'log')
-//      const n = Math.floor(Math.random() * 100)
+    it('counts down from n to 0', () => {
+      const spy = chai.spy.on(console, 'log')
+      const n = Math.floor(Math.random() * 100)
 
-//      expect(whileLoop(n)).to.equal('done')
-//      expect(spy).to.have.been.called.exactly(n)
+      expect(whileLoop(n)).to.equal('done')
+      expect(spy).to.have.been.called.exactly(n)
 
-//      console.log.reset()
-//    })
+      console.log.reset()
+    })
   })
 
   describe('doWhileLoop(array)', () => {
-//    it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
-//      const [array, t] = makeArray()
-//      const l = array.length
+    it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
+      const [array, t] = makeArray()
+      const l = array.length
 
-//      const newArray = doWhileLoop(array)
+      const newArray = doWhileLoop(array)
 
-  //    expect(newArray).to.have.length.of.at.most(l - 1)
-  //  })
+      expect(newArray).to.have.length.of.at.most(l - 1)
+    })
   })
 })
