932ccdb147ce783c278ae8239dcd5076f83a4d79
Robert Marais <robertmarais13@gmail.com>
2017-03-08 01:28:02 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,3 +4,9 @@ function forLoop(array){
   else{ array.push( `I am ${i} strange loops.`)}
 } return array
 }
+
+function whileLoop(number){while(number > 0){
+  console.log( -- number)
+}
+return 'done.'
+}
