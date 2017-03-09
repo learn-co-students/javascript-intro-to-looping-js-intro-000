0fe9d4fb1c538b98977d038f30aba3ecf77ccee2
Tim Stahl <timothystahl@gmail.com>
2017-03-09 04:33:01 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am a strange loop")
+    } else {
+      array.push(' I am ${i} strange loops.')
+    }
+  }
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done';
+}
+
+
+
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+do {
+  array = array.slice(1)
+
+} while (array.length > 0 && maybeTrue())
+
+return array
+}
