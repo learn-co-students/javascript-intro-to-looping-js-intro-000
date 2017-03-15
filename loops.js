c5d5c0057a2957b0e389ea1d526a89a0c5136b1c
David Lohmann <davelohmann@gmail.com>
2017-03-15 02:49:32 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i===0) {
+      array.push('I am ' + i + ' strange loop.')
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(num) {
+  while (num >0) {
+    console.log(--num)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
