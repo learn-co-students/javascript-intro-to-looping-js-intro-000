0503fb786649872c9d8f75ee74d66eab26f44e33
Juli James <jules57@gmail.com>
2017-03-21 18:47:13 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+   for (var i = 0; i < 26; i++) {
+     if(i == 1) {
+       return 'I am 1 strange loop.'
+     }
+     else {
+       return `I am ${i} strange loops.`
+     }
+  }
+}
+
+function whileLoop(n) {
+
+}
+
+function doWhileLoop(array) {
+
+}
