c2035bb5e71ca52d9f0d281f9bd32af1e2b57fa9
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-03-28 21:40:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,34 @@
+function forLoop(array){
+  for (var i =0; i<25; i++){
+  if (i == 1) {
+    array.push( "I am 1 strange loop." );
+  } else{
+    array.push( `"I am ${i} strange loops."` );
+    }
+  }
+  return array;
+}
+
+
+function whileLoop(n){
+  while(n > 0){
+  console.log(n--);
+
+  if(n === 0){
+    return "done";
+    }
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    console.log("And I ran; I ran so far away!");
+    array.pop();
+  } while(maybeTrue());
+
+  return array;
+}
