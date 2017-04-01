477bf7a85159488925ff4775f6c9a12a9a407ce7
Jessica Pamanian <jexeones@gmail.com>
2017-04-01 23:19:42 +0000

    Done.

--- loops.js
+++ loops.js
@@ -13,12 +13,14 @@ function whileLoop(number) {
   return 'done';
 }
 
+function maybeTrue(){
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
 do {
-  array.pop;
-}  while (function maybeTrue() {
-  return array.length > 0 && maybeTrue()
-});
+  array.pop();
+}  while (array.length > 0 || maybeTrue());
 
 return array;
 }
