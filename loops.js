27deb1eb9bfd7714c8c7dd5ca4450f9735bc661e
Landon Morgan <lndndwrd@gmail.com>
2017-04-11 23:28:53 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
