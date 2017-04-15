a7f7332882ee6577e915e0ba188819943e8cb92d
sungyun kim <kimsungyun7@hotmail.com>
2017-04-15 22:30:02 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,28 +1,27 @@
-function forLoop(array) {
-  for (var i = 0; i < 25; i++) {
-    if (i === 1) {
-      console.log("I am 1 strange loop.");
-    } else {
-    console.log("I am ${i} strange loops.");
+
+function forLoop(array){
+  var array = []
+  for (var i = 50; i < 75; i++){
+    array.push("i am looping");
   }
+  return sentences;
 }
 
-
-
-function whileLoop(n) {
-  var n = 25;
-  while (n > 0) {
-  n = n - 1
-}
-return console.log("done");
-}
-
-function maybeTrue() {
-  return Math.random() >= 0.5
+function loopingFun(){
+  var i = 10;
+  var num;
+  while(i < 17){
+    num = i;
+    i++;
+  }
+  return num;
 }
 
-do {
-  console.log('doo-bee-doo-bee-doo')
-} while (maybeTrue());
-
-function doWhileLoop(array) {
+function alwaysLooping(num){
+  var love = []
+  do {
+    love.push("JavaScript")
+    num ++;
+  } while (num < 10);
+  return love;
+}
