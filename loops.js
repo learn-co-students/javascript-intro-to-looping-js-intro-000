75d9c0fb2c740c9988101ca5f259fa5f011d267b
Dustin Anderson <dandersonsoftware@outlook.com>
2017-05-16 18:00:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,28 @@
 function forLoop(array) {
-  for (var i = 0; i <= 25, i++) {
-    if(i == 1) {
-      console.log("I am 1 strange loop.")
+  for (var i = 1; i <= 25; i++) {
+    if(i === 1) {
+      array.push("I am 1 strange loop.")
     } else {
-      console.log('I am ${i} strange loops.') 
+      array.push(`I am ${i} strange loops.`) 
     }
   }
+  return array;
+}
+
+function whileLoop(n) {
+  var countdown = n
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return 'done';
+}
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (maybeTrue() && array.length > 0);
+  return array;
 }
