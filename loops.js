fc2c088406a2568d98dc4e11a364046a2e599b48
James Jelenko <jamesjelenko@gmail.com>
2017-05-16 00:55:27 +0000

    Done.

--- loops.js
+++ loops.js
@@ -6,13 +6,14 @@ function whileLoop(num){
 }
 
 function forLoop(array){
-  for (var i = 1; i < 26; i++){
+  for (var i = 1; i <= 25; i++){
    if (i === 1) {
      array.push(`I am 1 strange loop.`);
    } else {
      array.push(`I am ${i} strange loops.`);
    }
 }
+return array 
 }
 
 function maybeTrue() {
