97481e3ee9f2661d23b779af6dcec3d967cdb7f6
Margaret Hoagland <mehoagland@gmail.com>
2017-05-22 14:07:17 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++){
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+        array.push(`I am ${i} strange loops.`);
+  }
+} return array;
+}
+
+function whileLoop(n){
+  while (10 > n){
+    console.log("done");
+  }
+}
