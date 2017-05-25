bf42413815e01a1a924498ef959ac0fe9c258bc9
Margaret Hoagland <mehoagland@gmail.com>
2017-05-25 19:35:46 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,7 +9,20 @@ function forLoop(array) {
 }
 
 function whileLoop(n){
-  while (10 > n){
-    console.log("done");
+  while (n > 0) {
+    console.log(n--);
   }
+  return "done";
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array){
+  do {
+  array.shift();
+} while ((array.length > 0) && (maybeTrue()));
+  return array;
 }
