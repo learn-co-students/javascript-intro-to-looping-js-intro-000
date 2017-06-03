a1cb9c74096958bc041b37cd8387595003eb2516
Logan Emerson <logemerson@gmail.com>
2017-06-03 00:48:41 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,29 @@
 function forLoop(array){
-  for (val i = 0; i < 25; i++){
+  for (var i = 0; i < 25; i++){
     if (i === 1){
       array.push("I am 1 strange loop.");
     } else {
       array.push(`I am ${i} strange loops.`)
     }
+  };
+  return array;
+};
+
+function whileLoop(number){
+  var counter = number;
+  while (counter > 0){
+    console.log(counter);
+    --counter;
   }
+  return "done";
+};
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array.length -= 1;
+  } while (array.length > 0 && maybeTrue());
   return array;
 };
