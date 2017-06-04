7320d45006262ff00f01ff601c1cb3ad56da91c5
Máté Rajnai <ramate.rajnai@gmail.com>
2017-06-04 09:26:14 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function makeArray() {const array = []
+                      const t = Math.floor(Math.random() * 10)}
+
+
+
+function forLoop(array) {for (let i = 1; i < 26; i++) {if (i = 1) {array.push('I am 1 strange loop.')}
+                                                        else {array.push(`I am ${i} strange loops.`)}
+                              } return array}
+
+
+
+                              end()
