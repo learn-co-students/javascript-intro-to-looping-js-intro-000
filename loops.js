031111b03be2ef9806a40e89eaa72f0087351e83
Máté Rajnai <ramate.rajnai@gmail.com>
2017-06-04 09:29:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function makeArray() {const array = []
 
 
 
-function forLoop(array) {for (let i = 1; i < 26; i++) {if (i = 1) {array.push('I am 1 strange loop.')}
+function forLoop(array) {for (let i = 1; i < 25; i++) {if (i === 1) {array.push('I am 1 strange loop.')}
                                                         else {array.push(`I am ${i} strange loops.`)}
                               } return array}
 
