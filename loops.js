03a67bb18d73dd4dfe9a272ab415aac2099e1b10
Jonathan Mevs <jrmevs@umd.edu>
2017-06-09 00:57:06 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(arr){
+  for(let i = 1; i <= 25; i++){
+    if(i===1){
+      arr.push("I am 1 strange loop.");
+    }
+    else{
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr;
+}
+
+function whileLoop(num){
+  while(num > 0){
+    console.log(num--);
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(arr){
+  do{
+    arr.pop();
+  } while(arr.length > 0 || (maybeTrue()===false)){
+    arr.pop();
+  }
+  return arr;
+}
