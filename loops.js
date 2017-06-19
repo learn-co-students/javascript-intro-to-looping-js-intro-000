10dd101825696cb37f26a2dbdc97971d45ffe759
J C <jchoi5@outlook.com>
2017-06-19 20:56:04 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am " + i + " strange loop.")
+    } else {
+      array.push("I am " + i + " strange loops.")
+    }
+  }
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
