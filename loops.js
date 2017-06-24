b596b5ed8555fc1e7a805f6fd0e1159dcbcf1571
Hoi Ki Yuen <cyhk116@gmail.com>
2017-06-24 09:22:14 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,8 +1,15 @@
 function forLoop(array)
 {
-  for(let i=0; i<25 i++)
+  for (var i=0; i<25; i++)
   {
-    array.push(`I am ${i} strange loop`);
+    if (i===1)
+    {
+      array.push(`I am ${i} strange loop.`);
+    }
+    else
+    {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
   return array;
 }
@@ -11,20 +18,20 @@ function whileLoop(n)
 {
   while(n)
   {
-    console.log(n);
-    i--;
+    console.log(n--);
   }
   return "done";
 }
 
 function doWhileLoop(array)
 {
-  function maybeTrue()
-  {
-    return Math.random() >= 0.5;
-  }
-
   do {
     array.pop();
-  } while (maybeTrue()&& array.length>0);
+  } while (array.length > 0 || maybeTrue());
+  return array;
+  
+  function maybeTrue()
+  {
+    return Math.random() >= 0.5
+  }
 }
