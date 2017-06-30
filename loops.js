a9ce4021fdd48c16acb8c530bd2a43910f3dff7b
lydie  toussaint <lydietoussaint123@gmail.com>
2017-06-30 16:23:15 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,13 +1,15 @@
 
-function forLoop(array) {
-  for (var i = 0; i < array.length; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
-    } else {
-       array.push("I am " + i + " strange loop.")
-    }
+function forLoop (array) {
+for(var i = 0; i < 25; i++){
+if (i === 1)
+ {
+   array.push("I am 1 strange loop.")
+ }
+else  {
+  array.push("I am  strange loops.")
   }
-
+ }
+  return array;
 }
 function whileLoop(n){
   while(n>0){
@@ -17,10 +19,11 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array) {
-  do {
-    array.pop()
-  } while(array.length > 0 && maybeTrue())
-function maybeTrue() {
-  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  function maybeTrue() {
+   return Math.random() >= 0.5
+   }
+   do {
+    array.pop(1);
+    } while (array.length > 0 && maybeTrue());
+    return array;
   }
-}
