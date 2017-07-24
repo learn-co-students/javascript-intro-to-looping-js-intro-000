251e5765dc5ff017b8d80e6196bedcfd531cfaa7
Melissa Lonsdale <mlonsdale@xogrp.com>
2017-07-24 00:20:54 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,49 @@
+
+function forLoop (array) {
+  for (var i = 1; i < 25; i++)
+      if (i === 1) {
+        array.push("I am 1 strange loop.");
+    } else {
+        array.push ("I am ${1} strange loops.");
+    }
+      return [array, t];
+  }
+//FOR LOOP
+
+
+//"I am ${i} strange loop${i === 0 ? '' : 's'}."
+//WHILE LOOP
+let n = 100;
+function = whileLoop(n) {
+  while (n > 0) {
+      console.log(--n);
+  else
+    return ("done");
+  }
+}
+
+
+
+//DO WHILE LOOP
+function = doWhileLoop(array) {
+  do {
+
+  } while (true);
+}
+
+
+//
+  //for(i = 0; i < 25; i++) {
+    //if (i === 1) {
+      //array.push(I am 1 strange loop.)
+    //} else {
+      //array.push("I am" ${i} "strange loops.")
+    //}
+    //}
+//}
+
+
+// function 'forLoop'
+// takes an array as argument
+//uses a 'for' loop
+// start counting from 0
