6b20e7180560b82c13aff24575c1a53a628e3453
L North <lnordell@gmail.com>
2017-08-17 04:18:49 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,31 +1,42 @@
-function forLoop (array) {
-  for (var i = 0; i < 25; i++){
-    if (i === 1) {
-        var myString = `I am ${i} strange loop.`;
-    } else {
-        var myString = `I am ${i} strange loops.`;
-    }
-    array.push(myString);
+//SUPER TERSE ELEGANT VERSION
+function forLoop(array) {
+  for (var i = 1; i <= 25; i++) {
+	 array.push(`I am ${i} strange loop${i === 1 ? '': 's'}.`);
   }
-  return array;
+      return array
 }
 
-function whileLoop (n) {
+
+// WORDIER version
+/* 
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+  if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+      return array
+}
+*/
+
+function whileLoop(n) {
   while (n > 0) {
-  console.log(--n);
+    console.log(n)
+    n--;
   }
   return 'done'
 }
-
 
 function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-function doWhileLoop(array) {
+function doWhileLoop (array) {
   do {
     array.pop();
-    maybeTrue(); /* or entire MaybeTrue function could go here instead*/
-  } while (maybeTrue() && array.length > 0);
+    maybeTrue();
+  } while (array.length > 0 && maybeTrue())
   return array;
 }
