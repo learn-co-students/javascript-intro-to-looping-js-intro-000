ea588ffaa3cea4207b741150d796b7a7d869ba79
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-07 21:18:29 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+var array = ["Hi"]
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while ( n > 0) {
+    n--
+    console.log(n)
+    if ( n === 0) {
+    return('done')
+    }
+  }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+function doWhileLoop(array) {
+  do { array.pop();
+
+  } while (array.length >= 0 && maybeTrue())
+
+  return array;
+}
