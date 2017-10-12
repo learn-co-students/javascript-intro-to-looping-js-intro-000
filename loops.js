681ca2955e9cd50abcf1a5c085059f7fc241f987
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-12 11:05:20 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,19 +1,28 @@
 
 function forLoop(array){
-  for (let i = 1; i < 25; i++) {
-    array[i] = `"I am ${i} strange loop${i === 1 ? '' : 's'}."`
+  for (var i = 0; i < 26; i++) {
+    array[i] = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
   }
   return array
 }
 
+var n = 10
 function whileLoop(n){
   while(n > 0){
-    console.log(--n)
+    console.log(--n);
   }
+  return "done"
 }
 
+
 function doWhileLoop(array){
-  do{
-    array.pop()
-  } while (array.length > 0 && doWhileLoop())
+  var myArray =["ciao", "bye", "hello", "Help"];
+  function maybeTrue() {
+    return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  };
+  do {
+    array.pop();
+  }
+  while (array.length > 0 && maybeTrue());
+  return myArray
 }
