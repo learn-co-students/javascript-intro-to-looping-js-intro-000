bfaf81dd91842a48d43f558ab65fcd3776624b1a
Linda Chang <lindachang186@gmail.com>
2017-10-18 14:06:49 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+  for (let i === 0; i < 25; i++) {
+    if (i === 1) {console.log(`"I am 1 strange loop."`)}
+    else {console.log(`"I am ${i} strange loops."`)}
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return console.log("done")
+}
+
+function doWhileLoop(array){
+  return Math.random() >= 0.5
+}
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue())
+return array;
