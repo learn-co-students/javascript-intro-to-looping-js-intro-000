6545b5404f63912313ed5fa57e1cbc0916b71562
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-20 20:51:53 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(['I am 1 strange loop.'])
+    } else {
+      array.push([`I am ${i} strange loops.`])
+    }
+  }
+  return array
+}
