34cdddce3f65b9f25f1a30215ff6b685288d235f
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-23 15:43:38 +0000

    Done.

--- loops.js
+++ loops.js
@@ -11,18 +11,24 @@ function forLoop(array){
 
 function whileLoop(num){
   while (num > 0) {
-    num -= 1  // subtract one from number
+    num -= 1  // subtract one from number ("original argument")
     console.log(num)
   }
     return 'done'
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
 
-function doWhileLoop(array){
+function doWhileLoop(array) {
+  function maybeTrue() {
+    var randomNum = Math.random()
+    console.log(randomNum)
+    return randomNum >= 0.5  // if randomNum is greater than or equal to 0.5, then returns true
+  }
+
   do {
-  console.log('doo-bee-doo-bee-doo')
- } while (maybeTrue());
+    array = array.slice(1)  // removes at least one element (first) from array without changing original array
+  } while (array.length > 0 && maybeTrue()) // return array while array is not empty and maybeTrue() function returns true
+  // If maybeTrue() function returns false the "do-while" loop will stop and it will return the array removing
+  // at least one element.
+  return array
 }
