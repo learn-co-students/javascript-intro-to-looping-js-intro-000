449429af53be2210f27008ee52f8b90f9299462c
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 14:56:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while (n > 0){
+    console.log(--n);
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1  && maybeTrue());
+  return array;
+}
