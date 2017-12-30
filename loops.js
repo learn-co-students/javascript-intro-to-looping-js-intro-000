db5757896bc2c041efbc092ef7c578df6b7b04f4
abbasqureshi7 <abbasqureshi7@gmail.com>
2017-12-30 05:27:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+
+
+function forLoop(array) {
+for ( i = 0; i < 25; i++) {
+  if (i == 1) {
+     array.push("I am 1 strange loop.");
+  } else {
+  array.push(`I am ${i} strange loop.`);
+}
+}
+return array }
+
+function whileLoop(n) {
+while (n > 0) {
+  n--;
+  console.log(--whileLoop)
+
+}
+return "done"
+}
+
+
+  function doWhileLoop(array) {
+  function maybeTrue() {
+    Math.random() >= 0.5;
+  }
+    do {
+       array.pop();
+       return "false"
+    } while (array.length > 0 && maybeTrue());
+
+  }
