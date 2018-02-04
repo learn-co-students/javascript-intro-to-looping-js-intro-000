7a45964355e3c15808dd8f4d39685637774962b2
Jesen Tanadi <mail@jesentanadi.com>
2018-02-04 19:38:39 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop (array) {
+  for (let i = 0; i < 25; i++) {
+    switch(i) {
+      case 1:
+        array.push("I am 1 strange loop.");
+        break
+
+      default:
+        array.push(`I am ${i} strange loops.`);
+    }
+  }
+
+  return array;
+}
+
+function whileLoop(num) {
+  while(num > 0){
+    console.log(--num);
+  }
+  return "done";
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while(array.length > 0 || !maybeTrue());
+
+  return array;
+}
