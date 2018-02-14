40e3d6360db6a06bfa7273944e0ebfe313e3928a
Alex Segers <a.segers.dev@gmail.com>
2018-02-14 22:15:36 +0000

    Done.

--- loops.js
+++ loops.js
@@ -15,12 +15,14 @@ function whileLoop(n) {
   return 'done'
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
 function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
   do {
-    array.splice(-1)
+    array = array.slice(1)
   } while (array.length > 0 && maybeTrue())
+
+  return array
 }
