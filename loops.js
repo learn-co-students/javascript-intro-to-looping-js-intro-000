c4eeaa4f7179c2318c7d7e55a1b10cac39267706
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-03 20:41:28 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,20 +1,24 @@
 function forLoop(arr) {
-  for (var i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     arr.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
+  return arr;
 }
 
 function whileLoop(n) {
-  while (n >= 0) {
+  while (n > 0) {
     n--;
+    console.log(n);
   }
+  return 'done';
 }
 
 function doWhileLoop(arr) {
   function maybeTrue() {
-    return Math.random() >= 0.5
+    return Math.random() >= 0.5;
   }
   do {
     arr.pop();
-  } while (arr.length > 0 || maybeTrue());
+  } while (arr.length > 0 && maybeTrue());
+  return arr;
 }
