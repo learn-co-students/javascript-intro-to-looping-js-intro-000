bf54efb56118dae16e76332192cd705aac284d60
Christopher Agius <chrisagius@fastmail.com>
2018-04-03 23:38:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`);
+
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n -= 1;
+  }
+  return "done";
+}
+
+function doWhileLoop(array) {
+
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+
+  return array;
+
+}
