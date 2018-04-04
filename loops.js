5083e481699e3054d718743fcc77ea1e354cfa44
Mary-Kate James <marykatj@gmail.com>
2018-04-04 15:15:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,43 +1,25 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  var newArray = array[i];
-    array.push(i === 1 ? "I am 1 strange loop." : "I am ${i} strange loops.");
+    array.push(i === 1 ? "I am 1 strange loop." : "I am ${i} strange loops.")
   }
   return array;
 }
 
 function whileLoop(number) {
-  let countdown = number;
-  while (countdown > 0) {
-    console.log(--countdown);
+  while (number > 0) {
+    console.log(number--);
   }
   return 'done';
 }
 
 function doWhileLoop(array) {
+
   function maybeTrue() {
-  return Math.random() >= 0.5
+    return Math.random() >= 0.5
   }
 
   do {
-    array.pop();
-    return array;
-  } while (array.length > 0 && maybeTrue());
+    array.shift()
+  } while (array.length > 0 && maybeTrue() === true);
+  return array;
 }
-
-
- /*
-
-   describe('doWhileLoop(array)', () => {
-     it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
-       const [array, t] = makeArray()
-       const l = array.length
-
-       const newArray = doWhileLoop(array)
-
-       expect(newArray).to.have.length.of.at.most(l - 1)
-     })
-   })
- })
-
- */
