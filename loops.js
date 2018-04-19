ea69db9303181731750a6ab667006715b0033d9e
sharon <sharonoptsout@gmail.com>
2018-04-19 22:31:13 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,23 @@
 
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i===1) {array.push("I am 1 strange loop.");}
+    else {array.push("I am "+${i}+" strange loops.");}
+  }
+}
 
-for (let i = 0; i < 25; i++) {
-  if (i===1) {return("I am 1 strange loop.");}
-  else{return("I am ",${i}," strange loops.");}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return("done");
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  }
+do{
+  
+  }while(maybeTrue());
 }
