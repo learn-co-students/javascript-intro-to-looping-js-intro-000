5bf95471fa09ce717c1ad86f8a174b485ff67a72
Vali Rosu <valiinapoimea2018@gmail.com>
2018-04-23 10:58:04 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+ function forLoop(array) {
+   for(let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`);
+   }
+   return array;
+}
+
+function whileLoop(n) {
+  while(n) {
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+do {
+  array.pop();
+} while (array.length > 0 && maybeTrue());
+  return array;
+}
