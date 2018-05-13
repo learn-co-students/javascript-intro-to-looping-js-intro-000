c8b79ef9401ad82e5e581c0e6994f1339b42e548
Dan Preston <dsppreston@yahoo.com>
2018-05-13 12:48:28 +0000

    Done.

--- loops.js
+++ loops.js
@@ -8,3 +8,22 @@ function forLoop (array) {
     }
 return array;
 }
+
+function whileLoop (n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+return "done";
+}
+
+function doWhileLoop (array) {
+  function maybeTrue () {
+    return Math.random () >= 0.5
+  }
+}
+do {
+  array.pop();
+} while (array.length > -1 && maybeTrue());
+return array;
+}
