e6cf51b8f0a6875d43673e16c8abe642a6f6b152
Dan Preston <dsppreston@yahoo.com>
2018-05-13 18:44:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -18,10 +18,10 @@ return "done";
 }
 
 function doWhileLoop (array) {
-  function maybeTrue () {
-    return Math.random () >= 0.5
-  }
+  function maybeTrue() {
+  return Math.random() >= 0.5
 }
+
 do {
   array.pop();
 } while (array.length > -1 && maybeTrue());
