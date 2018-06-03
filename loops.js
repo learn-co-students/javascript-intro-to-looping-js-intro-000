eaf86fcf7e6f999c14d626e395b87837e8064340
Alexander Nelson <amvetserv@gmail.com>
2018-06-03 06:58:17 +0000

    Done.

--- loops.js
+++ loops.js
@@ -20,12 +20,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
- function maybeTrue() {
-  return Math.random() >= 0.5;
-}
- 
-do {
-  array.pop();
-} while (array.length >= 0 && maybeTrue());
-return array;
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+
+  do {
+    array.pop();
+  } while (array.length >= 0 && maybeTrue());
+  return array;
 }
