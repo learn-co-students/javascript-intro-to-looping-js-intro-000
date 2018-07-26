eda44b68890411717ff41297de85dead4745b0a3
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-26 11:26:03 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+// for loop
+function forLoop (array) {
+  var t = array.length;
+  for (var i = 0 + t; i < 25 + t; i++) {
+    array[i] = "I am ${i} strange loop${i === 0 ? '' : 's'}.";
+  }
+  return array;
+}
+
+// while loop
+function whileLoop(n) {
+  while (n >= 1) {
+    console.log(--n);
+  }
+return 'done';
+}
+
+// do - while loop
+function doWhileLoop (array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
