2ffd4b53e4b49648666b955db32a36722633aab6
Johnathon Wood <jaywood128@gmail.com>
2018-09-10 10:30:41 +0000

    Done.

--- loops.js
+++ loops.js
@@ -16,3 +16,17 @@ function whileLoop(n) {
   }
     return "done"
 }
+function doWhileLoop(array) {
+  var i = 0;
+    function incrementVariable() {
+      i = i + 0;
+    }
+      return array
+
+  }
+     do {
+       earn
+
+     }
+
+}
