0fe88c935d588f8b2e94f8d833f3671ee80c9646
Johnathon Wood <jaywood128@gmail.com>
2018-09-10 10:21:45 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
+}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+
+  }
+    return "done"
+}
