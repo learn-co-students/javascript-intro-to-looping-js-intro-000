42d4e4045955fb6a80cce77ad7e5812c02c27f24
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-27 17:07:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,34 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+  return array
+}
+
+function whileLoop(n) {
+  countdown = n
+  while (countdown > 0) {
+    console.log(countdown)
+    countdown --
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+	}
+
+	do {
+		array = array.slice(1);
+		incrementVariable();
+	} while (array.length > 0 && i < 5);
+
+	return array;
+}
