36bef8c25d143003906dbd36a575bf0df63eb552
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-14 20:44:09 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,21 @@ function forLoop(array) {
   }
   return array
 }
+
+function whileLoop(n) {
+  let countdown = n
+  while (countdown>0) {
+      console.log(--countdown)
+    }
+    return "done"
+}
+
+function doWhileLoop(array) {
+    let i = 0;
+    do {
+      array.pop()
+      i++
+        }
+      while (i < array.length)
+return array
+}
