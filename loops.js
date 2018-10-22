9675912f69e402bfd2dab770e94ea675b664b861
Nora Vogt <noraevogt@gmail.com>
2018-10-22 19:39:22 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while ( n > 0 ) {
+    console.log(--n);
+  }
+  return "done"
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > 0 && incrementVariable())
+  return array
+}
