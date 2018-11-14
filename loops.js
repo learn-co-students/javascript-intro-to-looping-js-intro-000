eceab34e3c38a7db60b1e0c23b07bf63fb622b66
Simon Kilroy <simonkilroy1@gmail.com>
2018-11-14 13:34:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array) {
+for (var i = 0; i < 25; i++) {
+  if (i === 1) {
+    array.push("I am 1 strange loop.")
+  } else {
+    array.push(`I am ${i} strange loops.`)
+  }
+}
+  return array
+}
+
+var n = 10
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.shift();
+  } while (array.length > 0 && incrementVariable());
+  return array
+}
