cc60f6dd41d500a5aff196a86558b45d25d41ef6
Christina Lan <christina.lan13@gmail.com>
2018-11-17 15:34:41 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return 'done';
+  }
+
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+  return array;
+}
