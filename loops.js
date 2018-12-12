4246844b063c5a8489753f25e85cd08ab5b9d90a
Matthew Lee <yleematthew@gmail.com>
2018-12-12 21:16:35 +0000

    Done.

--- loops.js
+++ loops.js
@@ -17,13 +17,14 @@ function whileLoop(number){
 }
 
 
-var i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-}
 
 function doWhileLoop(array){
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+  }
+
   do {
   array.pop()
 } while (array.length>0 && incrementVariable())
