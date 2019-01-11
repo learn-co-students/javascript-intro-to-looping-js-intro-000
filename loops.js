7c4279908b5040c3ef4613295be5bbc9aa9547ac
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-01-11 04:39:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+  for (var i =0; i < 25; i++) {
+    if( i === 1) {
+      array.push("I am " + i + " strange loop.");
+    }else {
+      array.push(`I am ${i} strange loops.`);
+
+    }
+
+  } return array
+
+}
+
+
+
+function whileLoop(number){
+  let countdown = number;
+  while (countdown > 0) {
+    console.log(--countdown);
+
+  }
+  return "done";
+}
+
+
+
+function doWhileLoop(array) {
+  do {
+   array.pop()
+ } while (array.length > 0);
+ return array
+}
