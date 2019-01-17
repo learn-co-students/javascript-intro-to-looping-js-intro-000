b6e35f45abe7ba3ff513c0599f2a93f4fc54c19e
Yingqi Chen <chanwk18@hotmail.com>
2019-01-17 01:16:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array){
+  for(var i=0;i<25;i++){
+    if (i===0||i===1){
+    array.push(`I am ${i} strange loop.`)
+}else{
+  array.push(`I am ${i} strange loops.`)
+}
+  }
+  return array;
+}
+
+function whileLoop(num){
+  while (num>0){
+    console.log(num--)
+  }return "done"
+}
+
+
+function doWhileLoop(array){
+  var i=0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do{
+    array.pop();
+  }
+  while(array.length > 0 && incrementVariable());
+  return array;
+}
