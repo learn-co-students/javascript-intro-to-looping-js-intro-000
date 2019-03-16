de3280575dac72a19bcf95fd454e5004ef2449a6
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-16 23:46:31 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (var i = 0;i<25;i++) {
-    if (i=1) {
+    if (i===1) {
     array.push('I am 1 strange loop')
     }
     else {
@@ -15,4 +15,17 @@ function whileLoop(n) {
   while (n>0) {
     console.log(--n)
   }
+  return 'done'
+}
+
+
+function doWhileLoop(int) {
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {console.log('I run once regardless.');
+} while (incrementVariable()<int)
 }
