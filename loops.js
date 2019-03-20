09b7ec6d6e38d8181a7ed36d49a17ee75f704829
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-20 01:11:28 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,22 +1,28 @@
-function forLoop(array){
-  for (let i = 0; i<25; i ++){
-    if (i === 0){
-    n = "I am 1 strange loop.";
-    }
-    else { 
-    n = `I am ${i} strange loops.`;
-    }
-  return [...array, n];
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`);
+     } else {
+       array.push(`I am ${i} strange loops.`);
+     }
   }
+  return array;
 }
 
 function whileLoop(n){
-  
+  let countdown = n; 
+  while (countdown > 0){
+     console.log(--countdown);
+  }
+  return 'done'
 }
 
 function doWhileLoop(num){
-  
+  function incrementVariable(num){
+    num = num++; 
+    return num; 
+  }
+  do {
+      console.log("I run once regardless.");
+  } while (incrementVariable() < num);
 }
-
- array = [element, ...array];
-  return array
