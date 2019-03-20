7af6ff713e9d4dc6799963ca0d3a0e82b79fe40c
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-20 00:41:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array){
+  for (let i = 0; i<25; i ++){
+    if (i === 0){
+    n = "I am 1 strange loop.";
+    }
+    else { 
+    n = `I am ${i} strange loops.`;
+    }
+  return [...array, n];
+  }
+}
+
+function whileLoop(n){
+  
+}
+
+function doWhileLoop(num){
+  
+}
+
+ array = [element, ...array];
+  return array
