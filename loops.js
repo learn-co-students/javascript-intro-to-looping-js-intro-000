54aa55813bd15f36120876bfa6e3ef6ef6d88157
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-04-10 06:08:24 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else  {
+       array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+
+function  whileLoop(number) {
+  while (number!==0) {
+   console.log(number);
+   number--;
+  }
+  return 'done';
+}
+
+function doWhileLoop(integer) {
+  var i = 0; 
+  do {
+    i = i + 1;
+    console.log("I run once regardless.")
+  }while(i<integer) 
+  
+}
+  
