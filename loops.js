5b1026879993914f992bf6c9e139864887c01cc6
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-21 12:45:47 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,31 +1,33 @@
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
-  }
-  return array;
+  for (let i = 0; i < 25; i++){
+    if (i === 1){
+      array.push(`I am 1 strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+    }
+    return array;
 }
 
 function whileLoop(n) {
   let countdown = n;
+  
   while (countdown > 0) {
-    console.log(--countdown)
+    console.log(--
+    countdown);
   }
   return "done";
-  }
-  
- var i = 0;
- 
-function incrementVariable() {
-  i = i + 1;
-  return i;
 }
+var i = 0;
 
- function doWhileLoop(num) {
-   do {
-     console.log("I run once regardless")
- } while (incrementVariable() <= num);
- }
+function doWhileLoop(num){
+  function 
+  incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    console.log("I run once regardless");
+  } while
+  (incrementVariable() <= num);
+}
