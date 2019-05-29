3974439e89d2774c86ad4df8541defbbfd264d77
sinduja <sindhu.38@gmail.com>
2019-05-29 13:42:16 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,18 +1,16 @@
-function forLoop(array)
-{
-  for (let i=0;i<=25;i++)
-  {
-    if(i===1)
-    {
-      array[i]="I am 1 strange loop.";
+function forLoop(array){
+  for (let i=0;i<=25;i++){
+
+    if(i===1){
+      array[i].push("I am 1 strange loop.");
     }
-    else {
-      array[i]="I am "+i"  strange loops";
+    else{
+      array[i].push("I am "+i"  strange loops");
     }
   }
   return array;
 }
-function  whileLoop( n)
+function  whileLoop(n)
 {
   while(n>0)
   {
