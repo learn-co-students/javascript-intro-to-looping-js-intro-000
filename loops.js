8638b7f040d4301222ed66ccb6d850ae17a831c6
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-05 16:48:32 +0000

    Done.

--- loops.js
+++ loops.js
@@ -14,7 +14,9 @@ function forLoop(array) {
 			}
 function doWhileLoop(num) {
   var i = 0
-  function incrementVariable() { i = i + 1 }
+  function incrementVariable() { i = i + 1 
+    return i
+  }
   do {
     console.log('I run once regardless') }
     while (incrementVariable() < num)
