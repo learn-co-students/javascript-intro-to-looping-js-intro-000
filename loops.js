8f0143c20c38238475707261a7de1f4dac76c838
Stephanie Grande <stephanie.grande34@gmail.com>
2019-06-07 16:09:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -3,8 +3,28 @@ function forLoop(array) {
     if (i === 1) {
       array.push('I am 1 strange loop.');
     }else {
-      array.push('I am ${i} strange loop');
+      array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return 'done';
+}
+
+function doWhileLoop(num) {
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+  do{
+    console.log('I run once regardless.');
+  }while (incrementVariable() < num);
 }
