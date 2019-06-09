ad7e41907b73cfc6181e2c5d3b4225f264114bb9
Gabriella Ilena <ilenagabriella@gmail.com>
2019-06-09 08:51:46 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (let i = 0; i < 25 ; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--)
+  }
+  return "done"
+}
+
+var i = 0
+
+function incrementVariable() {
+  i = i + 1
+  return i
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log ("I run once regardless.")
+  } while (incrementVariable() <= num)
+}
