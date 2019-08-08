aac5877228b59e38a9c0c0b2f899aad7ae17ef08
JaimieLee Barnes <barnes.jaimielee@outlook.com>
2019-08-08 03:38:42 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,30 +1,20 @@
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
-  }
-  return array;
+for (let i = 0; i < 25; i++) {
+  if (i === 1) {array.push("I am 1 strange loop.")}
+  else {array.push("I am ${i} strange loops.")}
+}return array;
 }
 
- function whileLoop(n) {
-  let countdown = n;
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)}
+  return "done"}
 
-     while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
-
- function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
   }
 
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
+function doWhileLoop(array) {
+  do {array.pop()}
+  while (array.length > 0 && maybeTrue());
+  return array}
