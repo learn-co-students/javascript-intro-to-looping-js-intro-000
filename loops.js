986bff20cbea8f6e932f77830001f6bc68cb0234
Sunny Niu <sunnyyingli@gmail.com>
2019-09-01 00:31:05 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,4 +1,24 @@
-function whileLoop(number > 0){
-  console.log('done');
-  number--;
+function whileLoop(number){
+
+  while(number > 0){
+    console.log(--number);
+  }
+  return 'done'
+}
+
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if(i === 1){
+      array.push("I am 1 strange loop.");
+    }else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+
+function doWhileLoop(num) {
+  do{
+    console.log(num--);
+  } while(num > 0)
 }
