a1ae5fc4ff7c3a7078f39d06029daf329fd81b56
Shashi Sadasivan <shashi.sadasivan@gmail.com>
2019-09-12 22:00:08 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i <= 1 ? '' : 's'}.`);
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+function doWhileLoop(num) {
+  do{
+    console.log('I run once regardless');
+    num--;
+  }while(num > 0);
+}
