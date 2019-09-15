19db3e0982cb67c924b0b06f08d8f8acbf5169e8
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-15 09:17:16 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+  for(var i = 0; i < 25; i++){
+    if(i === 1){
+      array.push(`I am ${i} strange loop.`)
+    }else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(num){
+  var countdown = num
+  while(countdown > 0){
+    console.log(countdown);
+    --countdown;
+    }
+    return "done"
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(int){
+  do{
+    console.log("I run once regardless.")
+  } while (incrementVariable() <= int)
+}
