6612e4e21e89ce7ff53909577ebe045a1214ef71
patrick hoehn <patrh_99@hotmail.com>
2019-09-29 20:33:20 +0000

    Done.

--- loops.js
+++ loops.js
@@ -15,14 +15,13 @@ function whileLoop(n){
   return "done"
 }
 
-var i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
-
 function doWhileLoop(num){
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do{console.log("I run once regardless.")}
-  while (incrementVariable() <= num)
+  while (incrementVariable() < num)
 }
