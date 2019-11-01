628d3dd6e5f28d55bc8f493bc0fe0d28b6bd679f
Parker Preyer <parker.preyer@gmail.com>
2019-11-01 01:45:50 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(arr) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push('I am 1 strange loop.')
+    } else {
+        arr.push(`I am ${i} strange loops.`);
+    };
+  };
+  return arr;
+};
+
+function whileLoop(num) {
+  num;
+  while (num > 0) {
+    console.log(num);
+    num--;
+  };
+  return 'done';
+};
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+};
+
+var i = -1;
+
+function doWhileLoop(int) {
+  do {
+    console.log('I run once regardless.')
+  } while (incrementVariable() < int);
+};
