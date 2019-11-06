6f7412ae22911b1e23b268fa8d224bfd5ce6a94b
David Pink <dvdpink@gmail.com>
2019-11-06 23:21:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  let countdown = n;
+
+    while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
+  }
+
+
+  function doWhileLoop (i){
+    var timesPrinted = 0
+ do {
+   console.log("I run once regardless.")
+ timesPrinted++
+} while (timesPrinted < i)
+}
