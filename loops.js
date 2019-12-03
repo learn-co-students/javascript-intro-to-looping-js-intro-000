16106aba15e58f4400f6693dfe54da86875c2b84
Maxim Clefos <mk.clefos@gmail.com>
2019-12-03 00:03:52 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array){
   for(i=0; i<25; i++){
     if(i===1){
-      return "I am 1 strange loop."
+     array.push("I am 1 strange loop.")
     } else {
-      return `I am ${i} strange loops.`
+     array.push(`I am ${i} strange loops.`)
     }
-  }
+  } return array
 }
 
 function whileLoop(n){
@@ -15,7 +15,11 @@ function whileLoop(n){
 }
 
 function doWhileLoop(num){
-  num = num + 1;
-} do {
-  console.log("I run once regardless.")
-} while (num < 0)
+  var i = 0
+  function incrementVariable(){
+    i=i+1;
+    return i
+  } do {
+    console.log("I run once regardless")
+  } while (incrementVariable() < num)
+}
