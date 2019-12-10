683ad022862f772d63778f2bb50324b7e4a6e56a
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-10 04:31:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,14 +1,34 @@
 // forLoop takes an array as an input, and adds strings with a for loop.
 
-function forLoop(loopArray) {
-  for (var i = 1; i <= 25; i++) {
-    if i === 1 {
+function forLoop (loopArray) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
       loopArray.push("I am 1 strange loop.");
-      return loopArray
     }
     else {
       loopArray.push(`I am ${i} strange loops.`);
-      return loopArray
     }
   }
+  return loopArray
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number); // Decrements "number" by 1.
+  }
+  return "done"
+}
+
+function doWhileLoop(num) {
+  // Pick up incrementVariable from lessons
+  var i = 0;
+
+  function incrementVariable() {
+    i = i+1;
+    return i
+  }
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
 }
