344a5397250965aefe03c195d41093a60a44d27b
voluble-block-7446 <iagogomes80@gmail.com>
2020-01-01 20:39:13 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+var array=[];
+
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if(i===1){
+      array.push("I am 1 strange loop.");
+    }
+    else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+var n;
+function whileLoop(n){
+  while(n>=0){
+    n=n-1;
+    console.log(n);
+    if(n===0){
+      return "done";
+    }
+  }
+}
+
+var num;
+function doWhileLoop(num){
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do{
+    console.log("I run once regardless.");
+  }while(incrementVariable() < num);
+}
