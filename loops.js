2964086deaa3e0f0e83da2e52585e65227491171
Avera Guthrie <averaguthrie@gmail.com>
2020-02-04 22:43:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if(i = 1) {
+      return 'I am 1 stange loop.'
+    } else {
+      return `I am ${i} strange loops`
+    }
+  }
+  return array;
+}
