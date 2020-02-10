c25e45f75753acbd9ba4d066a8082014cdac930d
James Stamps Jr <jstampshere@gmail.com>
2020-02-10 16:30:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -22,15 +22,19 @@ return  "done"
 
 
 
+
 function doWhileLoop(num){
-  i = 0;
+  var i = 0;
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
   
 do {
   console.log("I run once regardless.");
   
   
-} while (`incrementVariable()` <= num);
-return i++;
-}
+} while (incrementVariable() < num);
 
+}
 
