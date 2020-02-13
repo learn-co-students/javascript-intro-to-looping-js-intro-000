0238c05be358eb0a93d4f871135edbb6964d171b
Mary Kang <mary.kang09@gmail.com>
2020-02-13 04:28:52 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+for (let i = 0; i<25; i +=1) {
+array.push(i === 1 ? 'I am 1 strange loop.' : `I am ${i} strange loops.`);
+}
+return array;
+}
+
+function whileLoop(n){
+while (n > 0) {
+      n-- ;
+      console.log(n)
+    }
+  return  "done"
+  }
+
+function doWhileLoop(num){
+  var i = 0
+ function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+return i++;
+}
