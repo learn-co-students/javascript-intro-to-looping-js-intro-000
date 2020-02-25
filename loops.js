e9df8721821dfa31548056efbaa8635ceb211e33
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 20:34:44 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+function forLoop(array){
+
+  for ( let i = 0; i < 25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`);
+    } else { 
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+   return array;
+}
+
+function whileLoop(number){
+  let countDown = number;
+  while (countDown > 0){
+    console.log(--countDown);
+  }
+  return 'done';
+}
+
+
+function doWhileLoop(num){
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  do{
+    console.log("I run once regardless.");
+  }while (incrementVariable() < num)
+}
