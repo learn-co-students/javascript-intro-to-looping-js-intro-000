a855c4d2e58b853eeb03775fe58e6ea9c786df54
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 15:03:31 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,32 +1,60 @@
 function forLoop(array) {
-for (var i = 0; i < 25; i++) {
-  if (i === 1) {
-    array.push("I am 1 strange loop.")
-  } else {
-    array.push(`I am ${i} strange loops.`)
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push (`I am ${i} strange loops.`)
+    }
   }
 }
-  return array
-}
 
-var n = 9
 
-function whileLoop(n) {
-  while (n > 0) {
-    console.log(--n)
-  }
-  return 'done'
-}
 
-var i = 0;
 
-function incrementVariable() {
-  i = i + 1;
-}
 
-function doWhileLoop(array) {
-  do {
-    array.shift();
-  } while (array.length > 0 && incrementVariable());
-  return array
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function forLoop(array) {
+// for (var i = 0; i < 25; i++) {
+//   if (i === 1) {
+//     array.push("I am 1 strange loop.")
+//   } else {
+//     array.push(`I am ${i} strange loops.`)
+//   }
+// }
+//   return array
+// }
+//
+// var n = 9
+//
+// function whileLoop(n) {
+//   while (n > 0) {
+//     console.log(--n)
+//   }
+//   return 'done'
+// }
+//
+// var i = 0;
+//
+// function incrementVariable() {
+//   i = i + 1;
+// }
+//
+// function doWhileLoop(array) {
+//   do {
+//     array.shift();
+//   } while (array.length > 0 && incrementVariable());
+//   return array
+// }
