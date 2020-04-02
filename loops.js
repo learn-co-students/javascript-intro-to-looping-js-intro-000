fd470cf5182256c764c9e9c65e553085186558a1
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-02 16:49:32 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,30 +1,27 @@
 function forLoop(array) {
-  for(let i=0; i < 25; i++) {
+  for(i=0; i < 25; i++) {
     if (i === 1) {
-      array.push(`I am 1 strange loop.`)
+      array.push(`I am 1 strange loop.`);
   }else {
     array.push(`I am ${i} strange loops.`)
   }
-return array}
-
-
-function whileLoop(n){
-  let count = n
-  while (n > 0) {
-    console.log(n)
   }
-  return 'done';
+return array;
 }
 
+function whileLoop(n){
+  let countdown=n
+  while(countdown>0) {
+    console.log(--countdown)
+    }
+    return 'done'
+  }
 
 
 
 
-
-
-
-let i = 0;
 function doWhileLoop(i){
+  let i = 0;
   function incrementVariable() {
     i = i + 1;
     return i;
@@ -33,4 +30,5 @@ function doWhileLoop(i){
   do {
     console.log("I run once regardless");
   } while (incrementVariable() < 10);
+  return i;
 }
