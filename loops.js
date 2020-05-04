662786dd8dc1e4997f0bf9d73eff8de8ea789707
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-04 14:34:37 +0000

    Done.

--- loops.js
+++ loops.js
@@ -13,7 +13,38 @@
 // Once the loop has finished, return the array full of strings.
 
 function forLoop(array) {
-  for(let i = 0; i < 25; i++) {
-    console.log(`I am ${i} strange loop.`)
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
   }
+  return array;
+}
+
+// **TODO**: Create a function called `whileLoop` in `loops.js`. The function
+// should take a number as an argument. Using a `while` loop, count down (using
+// `console.log`) from the passed in number to 0. Then return the string `'done'`
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(num--);
+  }
+  return "done";
+}
+
+// **TODO**: Define a function called `doWhileLoop` in `loops.js`. The function
+// should take an integer as an argument. Use the `incrementVariable()` function
+// (you can copy it from this README) in the condition, and console log
+// `"I run once regardless."` while `incrementVariable()` returns a number less
+// than the parameter received. (Your condition might look something like
+// `incrementVariable() < num`.) Remember that it should also console log when
+// receiving 0 as a parameter because the do-while runs before the condition is
+// checked.
+
+function doWhileLoop(int) {
+  function decrementVariable() {
+    return int--
+  }
+
+  do {
+    console.log("I run once regardless");
+  } while (decrementVariable() > 1);
 }
