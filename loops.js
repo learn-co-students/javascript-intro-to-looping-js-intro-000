0d23694162384b4c3e65c2375806d4b3cfdee576
Harold Lee <harry.yg.lee@gmail.com>
2020-05-12 14:48:05 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array){
-  for (let i=0;i<25;i++)
+  for(let i=0;i<25;i++)
   if(i===1){
     array.push('I am 1 strange loop.')
   }
@@ -8,20 +8,23 @@ function forLoop(array){
   }
   return array
 }
+
 function whileLoop(n){
+  let countdown='n'
   while(n>0){
     console.log(--n)
   }
   return 'done'
 }
-function doWhileLoop(num) {
-  let i = 0
+function doWhileLoop(num){
+  let i = 0;
+
   function incrementVariable() {
     i = i + 1;
     return i;
   }
 
   do {
-    console.log("I run once regardless.")
-  } while (incrementVariable() < num)
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
 }
