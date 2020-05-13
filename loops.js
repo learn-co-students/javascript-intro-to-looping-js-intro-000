a556c2abc8ad884f90e0cd25889bb34ba72a56f9
Brett Wilson <wilson.3458@osu.edu>
2020-05-13 22:45:40 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop${i === 1 ? '':'s'}.`);
+  }
+  return array;
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(--n);
+  }
+  return 'done';
+}
+
+let i = 0;
+function incrementVariable() {
+  i++;
+  return i;
+}
+
+function doWhileLoop(num){
+  do {
+    console.log('I run once regardless.');
+  } while(incrementVariable() <= num);
+}
