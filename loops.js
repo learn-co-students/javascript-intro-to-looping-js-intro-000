b9b3eef8c3c9c799c5333cceb55e11f5e1b1799e
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-12 00:02:49 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop (array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push ("I am 1 strange loop.");
+    }
+    else {
+      array.push (`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+function whileLoop(n) {
+  let countdown = n;
+    while (countdown > 0) {
+      console.log(--countdown);
+    }
+    return "done";
+}
+
+function doWhileLoop(num) {
+  let i = 1
+  do{
+    console.log("I run once regardless. ");
+  }while (i++ < num);
+}
