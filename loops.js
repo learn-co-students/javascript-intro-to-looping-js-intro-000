de157f8619ccba371b5e9e628d23936a6ad8e731
Colter Ulrich <cju5025@gmail.com>
2020-06-22 19:12:52 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,32 @@
-for (let i = 0; i < 25; i++){
-  if (i= 1) {
-    console.log("I am 1 strange loop.")
-  }  else {
-    console.log(`I am ${i} strange loops.`)
-  }
+function forLoop(array) {
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
+
+	return array;
+}
+
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
+
+	return 'done';
+}
+
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
 }
