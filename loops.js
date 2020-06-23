528ea47c2f3a5c13b073434f4dbf2b468ede8c76
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-23 21:53:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push("I am 1 strange loop.");
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr;
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(num)
+    num--
+  }
+  return 'done';
+}
+
+let i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log("I will run regardless");
+  } while (incrementVariable() <= num);
+}
