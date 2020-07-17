99760e9f2c13f3b59620f6b24d7e0918d11f285b
Riley McMudPie <rydonamusic@gmail.com>
2020-07-17 01:37:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop (array){
+  for (let i = 0; i < 25; i++){
+    if (i === 1){
+      array.push(`I am 1 strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  } return array;
+}
+
+function whileLoop(num){
+  while (num > 0){
+    console.log(num--)
+  } return 'done'
+}
+
+function doWhileLoop(num){
+  let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+} do {
+  console.log('I run once regardless.')
+} while (incrementVariable() < num)
+}
