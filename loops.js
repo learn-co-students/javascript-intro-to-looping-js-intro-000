1b55e1d3cf7c3ebc30da65e7fba3ad680b7d7523
Steven Quach <stevendquach@gmail.com>
2020-09-01 09:40:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,14 @@ function forLoop(array) {
   }
   return array;
 }
+
+function whileLoop(number) {
+  while (number > 0){
+    console.log(--number);
+  }
+  return ('done')
+}
+
+function doWhileLoop (num){
+
+}
