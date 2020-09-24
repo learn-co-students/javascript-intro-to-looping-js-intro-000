3a1aadddc0d4c6da79d9cc2ba6a7b249f15db264
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:08:03 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,27 @@
 function forLoop(array) {
-  for (let i=0; i<25; i++) {
-   if(i===1) {
-     array.push("I am 1 strange loop.")
-   } else {
-     array.push(`I am ${i} strange loops.`)
-   }
-}
-return array
+  for(i=0; i<25; i++) {
+    if(i==1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
 }
 
 function whileLoop(n) {
-  while (n > 0) {
-  console.log(n);
-  n-=1
-}
-return "done"
+  while (n>0) {
+  console.log(n)
+        n-=1
+      }
+    return "done"
 }
 
 function doWhileLoop(num) {
-  let i = 0;
+  var n = 0
   do {
   console.log("I run once regardless.")
-        i++
+        n++
 }
-while (i<num);
+while (num > n);
 }
