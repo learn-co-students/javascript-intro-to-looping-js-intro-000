6998513d6efc05cdf8715f76157945f0c0977488
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-11 00:49:52 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return 'done';
+}
+
+function doWhileLoop(n) {
+  let i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+  do {
+    console.log("I am run once regardless.");
+  } while (incrementVariable() < n);
+}
