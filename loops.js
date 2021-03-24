363e79648ee9bc390ad1d84e7003c13e27d7aa2d
Myles Starr <mylesstarr@gmail.com>
2021-03-24 20:48:11 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,34 @@
+function forLoop(array) {
+    for (let num = 0; num < 25; num++) {
+          if (num === 1) {
+                  array.push("I am 1 strange loop.");
+          } else {
+                array.push(`I am ${num} strange loops.`);
+          }
+    }
+
+    return array;
+}
+
+function whileLoop(n) {
+        while (n >0) {
+                console.log(--n);
+        }
+
+        return "done";
+}
+
+
+
+function doWhileLoop(num) {
+  let i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+  do{
+  console.log("I run once regardless.");
+} while (incrementVariable() < num );
+}
