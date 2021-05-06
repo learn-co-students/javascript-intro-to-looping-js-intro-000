d4304f5612aba68aef33800ff90bfe81d18be304
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 19:09:01 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,24 +1,26 @@
-const forLoop = arr => {
-  for (var i = 0; i < arr.length; i++) {
-    if (i == 1) arr.push("I am 1 strange loop.")
-    else arr.push(`I am ${i} strange loops.`);
+function forLoop (array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
   }
+  return array;
 }
 
-const whileLoop = n => {
-  while(n>0){
-    console.log(n)
-    n++;
+function whileLoop(number) {
+  while(number > 0) {
+    console.log(number);
+    number--;
   }
+  return "done";
 }
 
+let i = -1;
 function incrementVariable() {
   i = i + 1;
   return i;
 }
 
-const doWhileLoop = i => {
+function doWhileLoop(num) {
   do {
     console.log("I run once regardless.")
-  } while (incrementVariable() < i);
+  } while (incrementVariable() < num)
 }
